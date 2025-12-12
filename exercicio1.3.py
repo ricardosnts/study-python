5a9acf6e53b82975bc03676bd4894cdb91006d65
Ricardo Mol <ricardomolcsantos@gmail.com>
2025-12-12 10:10:35 -0300

    exercicios

---
+++ exercicio1.3.py
@@ -0,0 +1,6 @@
+n1 = float(input('Digite um número: '))
+n2 = float(input('Digite outro número: '))
+
+media = (n1+n2)/2
+
+print(f'A media dos números é igual a {media}!')
---
+++ exercicioifelse.py
@@ -0,0 +1,11 @@
+a = float(input('Digite um valor para a: '))
+
+if a == 10:
+    print("Hello")
+    print("Hi")
+elif a <= 6:
+    print("Indore")
+elif a == 5:
+    print("Goa")
+else:
+    print("Incorrect")
